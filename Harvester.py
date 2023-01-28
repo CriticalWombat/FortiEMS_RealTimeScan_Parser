f2cece8c2d2f6f88c5971e7b99a008359e4f6bac
Luke <lukevonhagel@nexigen.com>
2023-01-28 13:52:54 -0500

    Added handling for common errors

--- Harvester.py
+++ Harvester.py
@@ -2,57 +2,79 @@ import zipfile
 import os
 import patoolib
 import xlsxwriter
+from termcolor import colored
 
-class grainWrangler:
+
+class FortiEMS_AV_Parser:
     def __init__(self):
-        self.WB=xlsxwriter.Workbook('GrainReport.xlsx')
+        self.WB=xlsxwriter.Workbook('AV_Events.xlsx')
         self.WS=self.WB.add_worksheet()
         self.WS.write('A1', 'Hostname')
         self.WS.write('B1', 'Events')
-
         self.row=0
         self.col=0
 
-        for zip in os.listdir('.'):
-            if zip.endswith('.zip'):
-                hostname = self.__extractor(zip)
-                self.__cleanerUpper(zip)
+        for file in os.listdir('.'):
+            if file.endswith('.zip'):
+                self.__zipExtractor(file)
+            else:
+                continue
+
+        for file in os.listdir('.'):
+            if file.endswith('.cab'):
+                hostname = file.split("_")[1]
+                self.__cabExtractor(file, hostname)
                 events = self.__logWrangler(hostname)
                 self.__excelOps(hostname, events)
+            else:
+                continue
+        self.__removeExtracted()
         self.WB.close()
+            
+
 
     def __logWrangler(self, hostname):
         log = f'{hostname}\FCDiagData\general\logs\\realtime_scan.log'
-        with open(log, 'r') as rts:
-            lines = rts.readlines()
-            eventlist = []
-            for row in lines:
-                key="virus"
-                if row.find(key) != -1:
-                    eventlist.append(row)
-            return eventlist
+        if os.path.isfile(log):
+            with open(log, 'r') as rts:
+                lines = rts.readlines()
+                eventlist = []
+                for row in lines:
+                    key="virus"
+                    if row.find(key) != -1:
+                        eventlist.append(row)
+                return eventlist
+        else:
+            error = colored(f'No Real Time Scan log found for {hostname}...', 'red')
+            print(error)
+            
 
-    def __extractor(self, zip):
+    def __zipExtractor(self, zip):
         with zipfile.ZipFile(zip, mode="r") as zip:
-            zip.extractall()
+            return zip.extractall()
 
-        for cab in os.listdir('.'):
-            if cab.endswith('.cab'):
-                hostname=cab.split("_")[1]
-                patoolib.extract_archive(cab, outdir=hostname)
-                return hostname
-
-    def __cleanerUpper(self, zip):
+    def __cabExtractor(self, file, hostname):
+            try:
+                patoolib.extract_archive(file, outdir=hostname, verbosity=-1)
+                return
+            except:
+                error = colored(f'Issue with extracting {hostname}, please review folder manually!', 'red')    
+                print(error)
+                os.rename(hostname, "ERROR-{hostname}".format(hostname=hostname))
+                return
+            
+            
+    def __removeExtracted(self):
         for cab in os.listdir('.'):
             if cab.endswith('.cab'):
                 os.remove(cab)
-#            os.remove(zip)
 
     def __excelOps(self, hostname, events):
-        for virus in (events):
-            self.WS.write(self.row, self.col, hostname)
-            self.WS.write(self.row, self.col +1, virus)
-            self.row +=1
+        if events:
+            for virus in (events):
+                self.WS.write(self.row, self.col +1, hostname)
+                self.WS.write(self.row, self.col +1, virus)
+                self.row +=1
 
 if __name__=='__main__':
-    grainWrangler()
+    FortiEMS_AV_Parser()
